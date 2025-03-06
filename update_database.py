6345b244a94f31c81caec215eeb051073b7c8668
Joan Camps <kmps84@gmail.com>
2025-03-06 14:05:13 +0100

    add new function: save to a txt file unique list of clients and description items

--- update_database.py
+++ update_database.py
@@ -80,16 +80,16 @@ def apply_filter(driver, filters):
         field.send_keys(value)
         time.sleep(1)
     field.send_keys(Keys.RETURN)
-    time.sleep(5)
+    time.sleep(3)
     print("Filtros aplicados correctamente.")
 
 
 def download_file(driver):
     print("Descargando archivo...")
-    time.sleep(5)
+    time.sleep(3)
     download_button = driver.find_element(By.ID, "mx38-lb4")
     driver.execute_script("arguments[0].click();", download_button)
-    time.sleep(10)
+    time.sleep(45)
     print("Archivo descargado correctamente.")
 
 
@@ -122,6 +122,18 @@ def process_html_table(file_path):
     # Eliminar columna extra innecesaria
     print("Valores en la columna seleccionada como 'Planta':", df['Planta'].unique())
     print("Archivo procesado correctamente.")
+
+    # Guardar clientes únicos
+    unique_clients = [c.replace(' ', ' ').strip() for c in df['Cliente'].dropna().unique().tolist()]
+    with open("clientes_unicos.txt", "w", encoding="utf-8") as file:
+        file.write("\n".join(unique_clients))
+    print("Clientes únicos guardados en clientes_unicos.txt")
+
+    # Guardar descripciones únicas
+    unique_descriptions = [d.replace(' ', ' ').strip() for d in df['Descripción'].dropna().unique().tolist()]
+    with open("descripciones_unicas.txt", "w", encoding="utf-8") as file:
+        file.write("\n".join(unique_descriptions))
+    print("Descripciones únicas guardadas en descripciones_unicas.txt")
     return df
 
 
@@ -173,7 +185,7 @@ if __name__ == "__main__":
     DEST_FOLDER = os.path.expanduser("~/Documents/Maximo")
     os.makedirs(DEST_FOLDER, exist_ok=True)
 
-    filters = {"mx38_tfrow_[C:4]_txt-tb": "=TMB"}  # Ajustar filtros según proyecto
+    filters = {"mx38_tfrow_[C:26]_txt-tb": "=LAB-BAD"}  # Ajustar filtros según proyecto
 
     driver = setup_driver()
     try:
